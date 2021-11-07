708a2135a8c525706b931b153b8a5517acc995b3
YoongdooNoh <yoongdoo0819@naver.com>
2021-11-08 00:03:28 +0900

    5 chap Tx, TxIn, TxOut

--- helper.py
+++ helper.py
@@ -43,3 +43,32 @@ def decode_base58(s):
         raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
     return combined[1:-4]
 
+def little_endian_to_int(b):
+    return int.from_bytes(b, 'little')
+
+def int_to_little_endian(n, length):
+    return n.to_bytes(length, 'little')
+
+def read_varint(s):
+    i = s.read(1)[0]
+    if i == 0xfd:
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        return little_endian_to_int(s.read(8))
+    else:
+        return i
+
+def encode_varint(i):
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+        

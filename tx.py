36762cd1091e6373da5f47fbee9162272fb0d73f
YoongdooNoh <yoongdoo0819@naver.com>
2021-12-03 17:43:23 +0900

    coinbase tx

--- tx.py
+++ tx.py
@@ -120,6 +120,22 @@ class Tx:
         self.tx_ins[input_index].script_sig = Script([sig, sec])
         return self.verify_input(input_index)
     
+    def is_coinbase(self):
+        if len(self.tx_ins) != 1:
+            return False
+        first_input  = self.tx_ins[0]
+        if first_input.prev_tx != b'\x00' * 32:
+            return False
+        if first_input.prev_index != 0xffffffff:
+            return False
+        return True
+    
+    def coinbase_height(self):
+        if not self.is_coinbase():
+            return None
+        element = self.tx_ins[0].script_sig.cmds[0]
+        return little_endian_to_int(element)
+    
 class TxIn:
 
     def __init__(self, prev_tx, prev_index, script_sig=None, sequence=0xffffffff):
